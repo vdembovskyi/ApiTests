17b765f91dcf8710a267e1662f7dc7a409b79723
v.dembovskyi <v.dembovskyi@levi9.com>
2019-08-29 16:33:07 +0300

    My improved API tests

--- src/main/java/restClient/TestConfigData.java
+++ src/main/java/restClient/TestConfigData.java
@@ -6,7 +6,7 @@ public class TestConfigData {
 
     }
 
-    public static final String ACCESS_CODE = "4/qgH5QGdnZ2Yq2_4CHt4MKnt8414RxvybVK7tnwOYODRIUC4fWvuP44gDNIpG-rIyADQAyPaPAHxbBbESopZfYPE";
+    public static final String ACCESS_CODE = "4/qgFY24OJibETTkzeyAKO6XknP6i8abZUydoDQBCIWH1qpih1EYwhE3n9bputRmVN4Hbh23xclpT3cQV-Gl17sV8";
     public static final String CLIENT_ID = "351043311527-m7lv5daihffh4o61o8jvh9vov16p91n5.apps.googleusercontent.com";
     public static final String CLIENT_SECRET = "IuRJH4wz7vEsvUBpYEC8VC-g";
     public static final String REDIRECT_URI = "http://localhost";
